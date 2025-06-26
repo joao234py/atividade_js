5e4d70288bce9ee322c184a9515056e8da5f77c8
João Arthur <joaodagameplay302@gmail.com>
2025-06-26 16:13:27 -0300

    atividade 7,8

---
+++ atividade7.js
@@ -0,0 +1,4 @@
+let num1 = 4;
+let num2 = 3;
+let multiplicacao = num1 * num2;
+console.log(multiplicacao);
---
+++ atividade8.js
@@ -0,0 +1,5 @@
+let num1=3;
+let num2=2;
+let divisao = num1 / num2;
+let numero_formatado = divisao.toFixed(2);
+console.log(numero_formatado);
